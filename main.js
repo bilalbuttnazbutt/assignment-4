11d1642df86bf328edf333788512716b2f1dc4ae
bilalbuttnazbutt <164246858+bilalbuttnazbutt@users.noreply.github.com>
2024-04-25 18:55:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var quote = "A person who never made a mistake never tride anything new";
+var author = "Albert Einstein";
+console.log("".concat(author, " once said, ").concat(quote));
---
+++ main.ts
@@ -0,0 +1,3 @@
+let quote: string = "A person who never made a mistake never tride anything new";
+let author: string = "Albert Einstein";
+console.log(`${author} once said, ${quote}`);
